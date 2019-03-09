8fd7241b579ecefcc20cb8790f616074dbf1f8da
muril <muril@LAPTOP-C8P5BO14>
2019-03-09 15:45:30 -0300

    Busca/java

---
+++ Estrutura de dados/src/br/com/estrutura/Busca.java
@@ -0,0 +1,85 @@
+package br.com.estrutura;
+
+
+public class Busca {
+
+	//Sequencial
+	
+	public static long start = System.nanoTime(); // contador nano
+	
+	public static long start1 = System.currentTimeMillis(); // contador mili
+	
+	
+	public static int buscaSequencial(){
+		
+		int vet[] = {5, 7, 8, 1, 4, 6, 11, 34};
+				
+		int tl = 7; // Tamanho vetori
+		
+		int valor = 8; // Valor a encontrar
+		
+		int i=0;
+		
+		
+		while ((i<tl) && (vet[i]!=valor)){
+		i++;
+		}
+		if ((tl==0)||(i>=tl)) i = -1;
+		
+		return i;
+		
+		}
+	
+	
+	
+	//Binaria
+	
+	
+	 public static boolean Binaria(){ 
+		 
+		 int x = 78;
+		 	
+			int dados[] = {2, 4, 78, 65, 32, 21, 45, 6};
+	        int n = dados.length;     
+	        int aux = 0;  
+	        
+	        for(int i = 0; i < n-1; i++){   
+	            for(int j = i+1 ; j < n; j++){       
+	                if(dados[i] > dados[j]){   
+	                    aux = dados[j];     
+	                    dados[j] = dados[i];     
+	                    dados[i] = aux;     
+	                }     
+	            }     
+	        }     
+	        int meio;     
+	        int inicio = 0;     
+	        int fim = dados.length-1;     
+	        while (inicio <= fim) {     
+	                 meio = (inicio + fim)/2;     
+	                 if (x == dados[meio])     
+	                          return true;     
+	                 if (x < dados[meio])     
+	                          fim = meio - 1;     
+	                 else     
+	                          inicio = meio + 1;     
+	        }     
+	        return false;     
+	    }     
+	
+	
+	 public static long finish = System.nanoTime(); // contador nano
+		long total = finish - start;
+		
+		
+	public static long finish1 = System.currentTimeMillis(); // contador mili
+		long total1 = finish1 - start1;
+		
+		
+	 
+	 	
+	
+	 
+	
+	
+}
---
+++ Estrutura de dados/src/br/com/estrutura/Teste.java
@@ -0,0 +1,24 @@
+package br.com.estrutura;
+
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		 
+		Busca b1 = new Busca();
+		
+		System.out.println("Ordem Sequencial: "+ b1.buscaSequencial());
+		
+
+		System.out.println("Ordem Binaria: "+ b1.Binaria());
+		
+		System.out.println("Tempo nanosegundos: "+ b1.total);
+		
+		System.out.println("Tempo milissegundos: "+ b1.total1);
+		//a
+		
+
+	}
+
+}
