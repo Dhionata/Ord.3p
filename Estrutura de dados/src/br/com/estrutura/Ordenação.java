96b106024f6abedb7f7bca7692cb9787152ac1d9
Dhionatã <xiyunagi@gmail.com>
2019-03-12 12:37:53 -0300

    alpha 0.2

--- Estrutura de dados/src/br/com/estrutura/Ordenação.java
+++ Estrutura de dados/src/br/com/estrutura/Ordenação.java
@@ -1,6 +1,17 @@
 package br.com.estrutura;
+import java.util.*;
+
 
 public class Ordenação {
 
-	//0.1alpha kk
+	public static void main(String[] args) {
+		//0.2 alpha kk
+	
+		String [] palavras = {"goiaba","melancia","ovo","pinga","Dhionatã"};
+
+		Arrays.sort(palavras);
+		for(String pl: palavras) {
+			System.out.println(pl);
+		}
+	}
 }
