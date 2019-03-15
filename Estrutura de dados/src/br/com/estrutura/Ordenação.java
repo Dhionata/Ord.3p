cd464d4b967de1b5926040363d588a0c25103484
Dhionatã <xiyunagi@gmail.com>
2019-03-15 10:35:48 -0300

    1.0 beta [ordenação]

--- Estrutura de dados/src/br/com/estrutura/Ordenação.java
+++ Estrutura de dados/src/br/com/estrutura/Ordenação.java
@@ -1,17 +1,30 @@
 package br.com.estrutura;
-import java.util.*;
 
+import java.util.*;
 
 public class Ordenação {
 
 	public static void main(String[] args) {
-		
-		//0.3 alpha kk
-	
-		String [] palavras = {"goiaba","melancia","ovo","pinga","Dhionatã"};
+
+		// 1.0 beta kk
+
+		String[] palavras = { "goiaba", "melancia", "ovo", "pinga", "Dhionatã" };
 
 		Arrays.sort(palavras);
-		for(String pl: palavras) {
+		for (String pl : palavras) {
+			System.out.println(pl);
+		}
+		for (int i = 0; i < palavras.length; i++) {
+			for (int j = 0; j < (palavras.length - 1); j++) {
+				if (palavras[j].length() > palavras[j + 1].length()) {
+					String aux;
+					aux = palavras[j];
+					palavras[j] = palavras[j + 1];
+					palavras[j + 1] = aux;
+				}
+			}
+		}
+		for (String pl : palavras) {
 			System.out.println(pl);
 		}
 	}
