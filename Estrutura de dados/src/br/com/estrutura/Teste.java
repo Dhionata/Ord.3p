667ac2e976f22d8cfe7343a255da2e0fe5270579
Dhionatã <xiyunagi@gmail.com>
2019-03-15 15:59:52 -0300

    beta 1.4

--- Estrutura de dados/src/br/com/estrutura/Teste.java
+++ Estrutura de dados/src/br/com/estrutura/Teste.java
@@ -2,32 +2,31 @@ package br.com.estrutura;
 
 import java.util.Arrays;
 
-
+//beta 1.4
 public class Teste {
 
 	public static void main(String[] args) {
 
 		Busca b1 = new Busca();
-		
-		String[] blabla = {"eusoufoda","Dhionatã","oi","pessoaslegaissempreselascão","ouxit"};
-		
 
-		//System.out.println("Ordem Sequencial: " + Busca.buscaSequencial());
+		String[] blabla = { "eusoufoda", "Dhionatã", "oi", "pessoaslegaissempreselascão", "ouxit" };
 
-		//System.out.println("Ordem Binaria: " + Busca.Binaria());
+		// System.out.println("Ordem Sequencial: " + Busca.buscaSequencial());
 
-		//System.out.println("Tempo nanosegundos: " + b1.total);
+		// System.out.println("Ordem Binaria: " + Busca.Binaria());
 
-		//System.out.println("Tempo milissegundos: " + b1.total1);
+		// System.out.println("Tempo nanosegundos: " + b1.total);
 
-		//System.out.println("Ordem de leitura: " + Busca.Ler());
-		
-		//System.out.println("Tempo nanosegundos: ");
-		
-		Ordenação.QuickSort(blabla, 0, blabla.length -1);
-		//Ordenação.BubbleSort(blabla);
-		//Ordenação.InsertionSort(blabla);
-		//Ordenação.SelectionSort(blabla);
+		// System.out.println("Tempo milissegundos: " + b1.total1);
+
+		// System.out.println("Ordem de leitura: " + Busca.Ler());
+
+		// System.out.println("Tempo nanosegundos: ");
+
+		Ordenação.QuickSort(blabla, 0, blabla.length - 1);
+		// Ordenação.BubbleSort(blabla);
+		// Ordenação.InsertionSort(blabla);
+		// Ordenação.SelectionSort(blabla);
 
 		System.out.println(Arrays.toString(blabla));
 	}
