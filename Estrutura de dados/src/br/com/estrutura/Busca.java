a062430f10a507c5c6b370411f62277f7086c004
Dhionatã <xiyunagi@gmail.com>
2019-03-15 15:49:58 -0300

    será que deu ruim?

--- Estrutura de dados/src/br/com/estrutura/Busca.java
+++ Estrutura de dados/src/br/com/estrutura/Busca.java
@@ -1,93 +1,155 @@
 package br.com.estrutura;
 
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.RandomAccessFile;
+
+import java.io.FileReader;
+
+import java.io.IOException;
+
 import java.nio.file.Files;
 
 import java.nio.file.Path;
 
 import java.nio.file.Paths;
 
+
 public class Busca {
 
-	// Sequencial
-
+	
 	public static long start = System.nanoTime(); // contador nano
-
+	
 	public static long start1 = System.currentTimeMillis(); // contador mili
+	
 
-	public static String Ler() {
-
-		//mudar para "caminho" conforme o caso kk (By: Dhio)
-		Path caminho2 = Paths.get("C:\\Users\\muril\\Desktop\\Diciona\\dici.txt");
-		Path caminho = Paths.get("D:\\Users\\xiyun\\Desktop\\Dicionário\\Portu.txt");
-
+	//51541
+	
+	public String [] VetorCriar() {
+		
+		String leitura = "C:\\Users\\muril\\Desktop\\Diciona\\dici.txt";
+		
 		try {
-			byte[] texto = Files.readAllBytes(caminho);
-
-			String leitura = new String(texto);
-
-			return leitura;
-
-		} catch (Exception erro) {
-
-		}
-		return null;
-	}
-
-	public static int buscaSequencial() {
-
-		int vet[] = { 5, 7, 8, 1, 4, 6, 11, 34 };
-
-		int tl = 7; // Tamanho vetori
-
-		int valor = 8; // Valor a encontrar
-
+			
+		FileReader arquivo = new FileReader(leitura);
+		
+		BufferedReader leitorLinhas = new BufferedReader(arquivo);
+		
+		String linha = null;
+		
+		linha = leitorLinhas.readLine();
+		
+		int tamanhoVetor = Integer.parseInt(linha);  // erro de leitura está aqui, lendo linha vazia 
+		
+		String[] vetorTxt = new String [tamanhoVetor + 1];
+		
 		int i = 0;
+		
+			while(linha != null ) {
 
-		while ((i < tl) && (vet[i] != valor)) {
-			i++;
+				
+				vetorTxt[i] = linha;
+				
+				linha = leitorLinhas.readLine();
+				
+				i++;
+				
+			}	
+			
+		
+		arquivo.close();
+		
+		return vetorTxt;
+
+		
+	}catch (IOException erro) {
+		
+		System.out.println("Erro ao ler arquivo: " + erro.getMessage());
+		
+		
+	}
+		return null;
+	
+			
+}
+	//
+
+	//Metodo para depois que a String entrar no vetor
+	
+	//Sequencial \\
+	
+	public static int buscaSequencial(){
+		
+		int vet[] = {5, 7, 8, 1, 4, 6, 11, 34};
+				
+		int tl = 7; // Tamanho vetor
+		
+		int valor = 8; // Valor a encontrar
+		
+		int i=0;
+		
+		
+		while ((i<tl) && (vet[i]!=valor)){
+		i++;
 		}
-		if ((tl == 0) || (i >= tl))
-			i = -1;
-
+		if ((tl==0)||(i>=tl)) i = -1;
+		
 		return i;
-	}
-	// Binaria
-
-	public static boolean Binaria() {
-
-		int x = 78;
-
-		int dados[] = { 2, 4, 78, 65, 32, 21, 45, 6 };
-		int n = dados.length;
-		int aux = 0;
-
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = i + 1; j < n; j++) {
-				if (dados[i] > dados[j]) {
-					aux = dados[j];
-					dados[j] = dados[i];
-					dados[i] = aux;
-				}
-			}
+		
 		}
-		int meio;
-		int inicio = 0;
-		int fim = dados.length - 1;
-		while (inicio <= fim) {
-			meio = (inicio + fim) / 2;
-			if (x == dados[meio])
-				return true;
-			if (x < dados[meio])
-				fim = meio - 1;
-			else
-				inicio = meio + 1;
-		}
-		return false;
-	}
-
-	public static long finish = System.nanoTime(); // contador nano
-	long total = finish - start;
-
+	
+	
+	
+	//Binaria
+	
+	
+	 public static boolean Binaria(){ 
+		 
+		 int x = 78;
+		 	
+			int dados[] = {2, 4, 78, 65, 32, 21, 45, 6};
+	        int n = dados.length;     
+	        int aux = 0;  
+	        
+	        for(int i = 0; i < n-1; i++){   
+	            for(int j = i+1 ; j < n; j++){       
+	                if(dados[i] > dados[j]){   
+	                    aux = dados[j];     
+	                    dados[j] = dados[i];     
+	                    dados[i] = aux;     
+	                }     
+	            }     
+	        }     
+	        int meio;     
+	        int inicio = 0;     
+	        int fim = dados.length-1;     
+	        while (inicio <= fim) {     
+	                 meio = (inicio + fim)/2;     
+	                 if (x == dados[meio])     
+	                          return true;     
+	                 if (x < dados[meio])     
+	                          fim = meio - 1;     
+	                 else     
+	                          inicio = meio + 1;     
+	        }     
+	        return false;     
+	    }     
+	
+	
+	 public static long finish = System.nanoTime(); // contador nano
+		long total = finish - start;
+		
+		
 	public static long finish1 = System.currentTimeMillis(); // contador mili
-	long total1 = finish1 - start1;
+		long total1 = finish1 - start1;
+		
+		
+	 
+	 	
+	
+	 
+	
+	
 }
