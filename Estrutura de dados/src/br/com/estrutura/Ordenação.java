1a1b2a034f62101fb42d2b41fa605f2a364df57a
Dhionatã <xiyunagi@gmail.com>
2019-03-15 12:48:19 -0300

    QuickSort (será que funfa? kk)

--- Estrutura de dados/src/br/com/estrutura/Ordenação.java
+++ Estrutura de dados/src/br/com/estrutura/Ordenação.java
@@ -36,22 +36,58 @@ public class Ordenação extends Busca {
 		for (String pl : palavras) {
 			System.out.println(pl);
 		}
-		
+
 		int i, j;
 		String aux;
-		
-		for(i = 1;i <palavras.length;i++) {
+
+		for (i = 1; i < palavras.length; i++) {
 			aux = palavras[i];
 			j = i - 1;
-			
-			while((j >= 0) && (aux.compareToIgnoreCase(palavras[j]) == -1)) {
-				palavras[j+1] = palavras[j];
+
+			while ((j >= 0) && (aux.compareToIgnoreCase(palavras[j]) == -1)) {
+				palavras[j + 1] = palavras[j];
 				j--;
 			}
-			palavras[j+1] = aux;
+			palavras[j + 1] = aux;
 		}
 		for (String pl : palavras) {
 			System.out.println(pl);
 		}
+	}
+
+	public void QuickSort(int[] p, int esquerdo, int direito) {
+		if (esquerdo < direito) {
+			int j = separar(p, esquerdo, direito);
+			QuickSort(p, esquerdo, j - 1);
+			QuickSort(p, j + 1, direito);
+
+		}
+	}
+
+	private int separar(int[] p, int esquerdo, int direito) {
+		int i = esquerdo + 1;
+		int j = direito + 1;
+		int pivo = p[esquerdo];
+
+		while (i <= j) {
+			if (p[i] <= pivo) {
+				i++;
+			} else if (p[j] > pivo) {
+				j++;
+			} else if (i <= j) {
+				trocar(p, i, j);
+				i++;
+				j--;
+			}
+		}
+		trocar(p, esquerdo, j);
+		return j;
+	}
+
+	private void trocar(int[] p, int i, int j) {
+		int aux = p[i];
+		p[i] = p[j];
+		p[j] = aux;
+
 	}
 }
