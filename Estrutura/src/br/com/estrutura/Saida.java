2b4d41e64446a1f8b22adee43144107ba555f439
Dhionatã <xiyunagi@gmail.com>
2019-03-22 14:41:15 -0300

    Arrumado a impressão da classe saida

--- Estrutura/src/br/com/estrutura/Saida.java
+++ Estrutura/src/br/com/estrutura/Saida.java
@@ -1,92 +1,28 @@
 package br.com.estrutura;
 
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintStream;
-
-import java.util.Scanner;
 
 public class Saida {
 
-	public static String[] metodoPorScan(String[] arquivo) throws IOException {
-		
-	try {
-		String nomedoindereco = null;
-		String[] arq = arquivo;
-		BufferedWriter imprimir = new BufferedWriter(new FileWriter(nomedoindereco));
-		String linha = "";
-		
-		for(int i = 0; i<= arq.length; i++) {
-			linha = arq[i] + " ------TAMANHO----- " + arq[i].length() +"\n";
-			imprimir.append(linha);
-		}
-		
-	} catch (FileNotFoundException e) {
+	public static String[] metodoPorScan(String[] arquivo) {
+
+		try {
+			String[] arq = arquivo;
+			PrintStream ps = new PrintStream("./Output/saída.txt");
+			String linha = "";
+
+			for (int i = 0; i <= arq.length; i++) {
+				linha = arq[i] + " ------TAMANHO----- " + arq[i].length() + "\n";
+				ps.print(linha);
+			}
+
+		} catch (FileNotFoundException e) {
 
 			e.printStackTrace();
 		}
-		
+
 		return null;
-		
-		
-//		Scanner sc = null;
-//		try {
-//
-//			// leitor
-//			for(int i = 0; i <= arq.length; i++) {
-//			sc = (new Scanner(new FileReader(arq[i])));
-//			}
-//			// escritor
-//
-//			PrintStream ps = new PrintStream("./Output/saída.txt");
-//
-//			// percorre o arquivo
-//
-//			while (sc.hasNextLine()) {
-//
-//				String linha = sc.nextLine();
-//
-//				// Comprimento da String
-//
-//				int imprimir = linha.length();
-//
-//				int length = imprimir;
-//
-//				// grava no destino
-//
-//				ps.print(linha);
-//				ps.println("----------------------------" + length);
-//
-//				System.out.println();
-//
-//			}
-//
-//			// fecha tudo
-//
-//			ps.close();
-//
-//			sc.close();
-//
-//		} catch (FileNotFoundException e) {
-//
-//			e.printStackTrace();
-//
-//		}
-//		return null;
-//	}
-//
-//	public static void main(String[] args) {
-//
-//		// gravando
-//
-//		// metodoPorStream();
-//
-//		metodoPorScan(null);
-//
-//	}
 	}
 }
